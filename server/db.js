6b6319dd6824934ac22233deb46a9a98f7b184be
Bernat Alcudia <bernatalcudia@gmail.com>
2025-04-29 10:56:07 +0200

    First commit

---
+++ server/db.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require("sequelize");
+const defineBookModel = require("./models/Book")
+
+const sequelize = new Sequelize("pruebadb", "root", "root", {
+    host: "localhost",
+    dialect: "mysql",
+})
+
+const db = {}
+db.books = defineBookModel(sequelize);
+db.sequelize = sequelize;
+
+
+module.exports = db;
